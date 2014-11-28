978a94b08203ed2f0274fba2427c2ff460e61b9b
dimitritoonen <dimitritoonen@gmail.com>
2014-11-28 19:23:02 +0100

    Set-up gulp and added Sass

---
+++ src/Chirping.Web/gulp/config.js
@@ -0,0 +1,63 @@
+﻿var src = 'src';
+var dest = 'build'
+var development = 'build/development';
+var production = 'build/production';
+
+module.exports = {
+  browsersync: {
+    development: {
+      server: {
+        baseDir: [dest]
+      },
+      port: 8080,
+      files: [
+        dest + '/**'
+      ]
+    }
+  },
+  del: {
+    dest: dest
+  },
+  sass: {
+    src: [src + '/bower_modules/bootstrap-sass-official/assets/stylesheets/**/*.{sass,scss}',
+    src + '/sass/**/*.{sass,scss}'],
+    dest: dest + '/css',
+    options: {
+      noCache: true,
+      compass: false,
+      bundleExec: false,
+      sourcemap: false
+      //sourcemapPath: '/sass'
+    }
+  },
+  autoprefixer: {
+    browsers: [
+      'last 2 versions',
+      'safari 5',
+      'ie 8',
+      'ie 9',
+      'opera 12.1',
+      'ios 6',
+      'android 4'
+    ],
+    cascade: true
+  },
+  baseUrl: {
+    dest: dest,
+    src: src
+  },
+  js: {
+    src: src,
+    dest: dest
+  },
+  images: {
+    src: src + '/images/**/*',
+    dest: dest + '/images/'
+  },
+  fonts: {
+    development: {
+      src: src + '/bower_modules/bootstrap/fonts/*',
+      dest: dest + '/fonts'
+    }
+  }
+};
