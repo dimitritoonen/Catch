d2757557f90709e9378de0bd7c163ba24f402744
dimitritoonen <dimitritoonen@gmail.com>
2014-12-12 09:22:01 +0100

    Added keypass

---
+++ src/Chirping.Web/src/app/config/acceptance.app.config.js
@@ -0,0 +1,13 @@
+﻿define(function () {
+
+  // defines the acceptance configuration used throughout the SPA
+
+  var configuration = {
+
+    // acceptance
+    BaseUrl: 'http://api-acceptance.chirping.nl'
+
+  };  
+  
+  return configuration;
+});
---
+++ src/Chirping.Web/src/app/config/development.app.config.js
@@ -0,0 +1,12 @@
+﻿define(function () {
+
+  // defines the development configuration used throughout the SPA
+
+  var configuration = {
+
+    BaseUrl: 'http://localhost:4421/'
+
+  };
+  
+  return configuration;
+});
