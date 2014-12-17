684b1a8a130e8b5cda7ab5f75b3ffdaca6cd52cd
dimitritoonen <dimitritoonen@gmail.com>
2014-12-17 22:10:05 +0100

    Added gulp tasks for less

---
+++ src/Chirping.Web/gulp/tasks/dev-build/less-vendor.js
@@ -0,0 +1,21 @@
+﻿var gulp = require('gulp');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var config = require('../../config').less.vendor;
+var cssConfig = require('../../config').css;
+
+// add plugins for piping vendor css files
+var concat = require('gulp-concat');
+var es = require('event-stream');
+
+gulp.task('less:vendor', function () {
+
+  var bowerFiles = gulp.src(cssConfig.src);
+  var lessFiles = gulp.src(config.src)
+    .pipe(less())
+    .pipe(gulp.dest(config.dest));
+
+  return es.concat(bowerFiles, lessFiles)
+    .pipe(concat('vendor-styles.css'))
+    .pipe(gulp.dest(config.dest));
+});
---
+++ src/Chirping.Web/gulp/tasks/dev-build/less.js
@@ -0,0 +1,29 @@
+﻿var gulp = require('gulp');
+var less = require('gulp-less');
+var browsersync = require('browser-sync');
+var gulpFilter = require('gulp-filter');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var config = require('../../config');
+
+// add plugins for piping css files
+var concat = require('gulp-concat');
+var es = require('event-stream');
+
+gulp.task('less', function () {
+
+  // Don’t write sourcemaps of sourcemaps
+  var filter = gulpFilter(['*.css', '!*.map']);
+  
+  var lessFiles = gulp.src(config.less.src)
+    .pipe(less())
+    .pipe(sourcemaps.init())
+    .pipe(filter) // Don’t write sourcemaps of sourcemaps
+    .pipe(sourcemaps.write('.', { includeContent: false }))
+    .pipe(filter.restore()) // Restore original files
+    .pipe(gulp.dest(config.less.dest));
+
+  return es.concat(lessFiles)
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest(config.less.dest));
+});
