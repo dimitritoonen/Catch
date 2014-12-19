bf44671435434bb18b44248b710c7c82a25126ad
dimitritoonen <dimitritoonen@gmail.com>
2014-12-19 16:30:11 +0100

    Add automation directory

---
+++ src/Chirping.Web/gulp/tasks/exclude-from-publish/watch.js
@@ -0,0 +1,12 @@
+﻿var gulp = require('gulp');
+var config = require('../../config').watch;
+
+// start the browsersync task and watch files for changes
+gulp.task('watch', ['browsersync'], function () {
+
+  gulp.watch(config.less, ['less', 'scsslint']);
+  gulp.watch(config.scripts, ['copy:scripts', 'jshint']);
+  gulp.watch(config.images, ['images']);
+  gulp.watch(config.html, ['html']);
+
+});
