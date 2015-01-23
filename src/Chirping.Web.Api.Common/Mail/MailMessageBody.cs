7f5e25a97b5bb05b372c4a7b0a7cf3495bb37560
dimitritoonen <dimitritoonen@gmail.com>
2015-01-23 09:00:45 +0100

    Added SendGrid

---
+++ src/Chirping.Web.Api.Common/Mail/MailMessageBody.cs
@@ -0,0 +1,23 @@
+﻿#region using directives
+
+using System;
+
+#endregion
+
+namespace Chirping.Web.Api.Common.Mail
+{
+    public class MailMessageBody
+    {
+        private string _mailMessage = "";
+
+        public MailMessageBody(string mailMessage)
+        {
+            this._mailMessage = mailMessage;
+        }
+
+        public override string ToString()
+        {
+            return _mailMessage;
+        }
+    }
+}
