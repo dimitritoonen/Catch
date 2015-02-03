a0eac0a7ec24b4566e96c3f44deb0756bfd9abef
dimitritoonen <dimitritoonen@gmail.com>
2015-02-03 18:07:08 +0100

    Added Profile again

---
+++ src/Chirping.Web.Api.Common/Data/Profile.cs
@@ -0,0 +1,37 @@
+﻿#region using directives
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#endregion
+
+namespace Chirping.Web.Api.Common.Data.Entities
+{
+    public class Profile
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        [Index(IsUnique=true)]
+        [MaxLength(50)]
+        public string NickName { get; set; }
+
+        [Required]
+        [MaxLength(2)]
+        public string Age { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string City { get; set; }
+
+        [Required]
+        public string InterestedIn { get; set; }
+
+        public string ProfileImage { get; set; }
+    }
+}
