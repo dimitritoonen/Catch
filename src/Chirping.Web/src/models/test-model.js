58a940a4b4e60fddf7fc48b6df7999a638bd780e
dimitritoonen <dimitritoonen@gmail.com>
2015-03-02 18:37:37 +0100

    Added tests

---
+++ src/Chirping.Web/src/models/test-model.js
@@ -0,0 +1,19 @@
+﻿define('jquery', 'knockout', function ($, ko) {
+
+  function TestModel() {
+
+    var self = this;
+
+    self.GetTestMethod = function () {
+
+      return $.ajax({
+        type: 'GET',
+        url: 'http://localhost:4421/api/activity'
+      });
+
+    };
+
+  }
+
+  return new TestModel;
+});
---
+++ src/Chirping.Web/test/components/test-model.js
@@ -0,0 +1,27 @@
+﻿define(['jquery', 'knockout', 'models/activity-model', 'mock-activities', 'jquery-mockjax'], function ($, ko, ActivityModel, mockActivities) {
+
+  describe('activity model', function () {
+
+    it('should test stuff', function (done) {
+      $.mockjax({
+        url: 'http://localhost:4421/api/activity',
+        responseTime: 10, responseText: mockActivities.GetActivities({})
+      });
+
+      // arrange
+      var instance = ActivityModel;
+
+      console.log('1');
+
+      setTimeout(function () {
+
+        console.log('2');
+        expect(instance.Activities().length).toEqual(1);
+
+        done();
+      }, 5);
+    });
+
+  }); // activity model
+
+});
