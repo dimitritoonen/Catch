35a84a7b365d10f2259f5cf79e5110d3b3f0f351
dimitritoonen <dimitritoonen@gmail.com>
2015-03-10 09:41:34 +0100

    Added register user tests

--- src/Chirping.Web.Api.Tests/Processors/AccountProcessorTest.cs
+++ src/Chirping.Web.Api.Tests/Processors/AccountProcessorTest.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
+using System.Configuration;
 using System.Threading.Tasks;
 
 namespace Chirping.Web.Api.Tests.Processors
@@ -28,6 +29,9 @@ namespace Chirping.Web.Api.Tests.Processors
             _profileImageStoreMock.Setup(x => x.StoreImage(It.IsAny<string>(), It.IsAny<string>()));
 
             _accountRepositoryMock = new Mock<IAccountRepository>();
+
+
+            ConfigurationManager.AppSettings["FrontEndUrl"] = "http://localhost:8080";
         }
 
         private Mock<IAutoMapper> _automapperMock;
@@ -36,15 +40,19 @@ namespace Chirping.Web.Api.Tests.Processors
 
 
         [TestMethod]
-        public void Can_Register_User()
+        public void Can_Register_User_With_Success()
         {
             // arrange
             RegisterBindingModel user = GetUser();
             UserAccount userAccount = GetUserAccount();
-            Task<RegisterUserResult> result = GetIdentityResult();
+            Task<RegisterUserResult> result = GetIdentityResult(true);
+            var confirmationToken = Task.FromResult<string>(Guid.NewGuid().ToString());
 
             _accountRepositoryMock.Setup(x => x.RegisterUser(It.IsAny<UserAccount>()))
                 .Returns(result);
+
+            _accountRepositoryMock.Setup(x => x.GenerateEmailConfirmationTokenAsync(It.IsAny<string>()))
+                .Returns(confirmationToken);
 
             _automapperMock.Setup(m => m.Map<UserAccount>(user)).
                 Returns(userAccount);
@@ -57,13 +65,41 @@ namespace Chirping.Web.Api.Tests.Processors
             // act
             Task<IdentityResult> task = processor.RegisterUser(user);
 
-            Console.WriteLine(task);
-            
             // assert
-            //Assert.IsTrue(task.Result.Succeeded);
-            //Assert.AreEqual<int>(task.Result.Errors.Count(), 0);
+            // verify if the appropriate methods are called (RegisterUser, SendEmail, and StoreImage)
+            _accountRepositoryMock.Verify(mock => mock.RegisterUser(It.IsAny<UserAccount>()), Times.Once());
+            _accountRepositoryMock.Verify(mock => mock.SendEmailAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+            _profileImageStoreMock.Verify(mock => mock.StoreImage(It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+        }
 
 
+        [TestMethod]
+        public void Register_User_Without_Success()
+        {
+            // arrange
+            RegisterBindingModel user = GetUser();
+            UserAccount userAccount = GetUserAccount();
+            Task<RegisterUserResult> result = GetIdentityResult(false);
+
+            _accountRepositoryMock.Setup(x => x.RegisterUser(It.IsAny<UserAccount>()))
+                .Returns(result);
+            
+            _automapperMock.Setup(m => m.Map<UserAccount>(user)).
+                Returns(userAccount);
+
+            var processor = new AccountProcessor(
+                _accountRepositoryMock.Object,
+                _profileImageStoreMock.Object,
+                _automapperMock.Object);
+
+            // act
+            Task<IdentityResult> task = processor.RegisterUser(user);
+
+            // assert
+            // verify if the appropriate methods are called (RegisterUser, SendEmail, and StoreImage)
+            _accountRepositoryMock.Verify(mock => mock.RegisterUser(It.IsAny<UserAccount>()), Times.Once());
+            _accountRepositoryMock.Verify(mock => mock.SendEmailAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never());
+            _profileImageStoreMock.Verify(mock => mock.StoreImage(It.IsAny<string>(), It.IsAny<string>()), Times.Never());
         }
 
         private UserAccount GetUserAccount()
@@ -97,11 +133,26 @@ namespace Chirping.Web.Api.Tests.Processors
             };
         }
         
-        private Task<RegisterUserResult> GetIdentityResult()
+        private Task<RegisterUserResult> GetIdentityResult(bool success)
         {
-            RegisterUserResult result = new RegisterUserResult();
+            return Task.FromResult<RegisterUserResult>(new RegisterUserResultFake(success));
+        }
+        
+        // fake classes
+        class RegisterUserResultFake : RegisterUserResult
+        {
+            public RegisterUserResultFake(bool success)
+            {
+                this.IdentityResult = new IdentityResultFake(success);
+                this.UserId = Guid.NewGuid().ToString("N");
+            }
+        }
 
-            return Task.FromResult<RegisterUserResult>(result);
+        class IdentityResultFake : IdentityResult
+        {
+            public IdentityResultFake(bool success)
+                : base(success)
+            {}
         }
     }
 }
